166ca97077af6b066d1cc924774321cc5d440828
ramy <ramyshurafa@hotmail.com>
2021-01-18 12:44:30 +0200

    api endpoints

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import { errorMsgs } from '../services/error-handler';
-import { test } from '../modules';
+import { users, checkIns, skills, activities, organisations } from '../modules';
 
 const router = Router();
 
-router.use('/test', test.controllers);
+router.use('/users', users.controllers);
+router.use('/check-ins', checkIns.controllers);
+router.use('/skills', skills.controllers);
+router.use('/activities', activities.controllers);
+router.use('/organisations', organisations.controllers);
 
 // catch 404 and forward to error handler
 router.use((req, res, next) => {
