8c2803fc30197afc0c2d37d029955231975fcac4
Ahmed A. Shatat <a.shatat@hotmail.com>
2021-01-18 15:27:02 +0200

    add LeftRow Icon
    #44

--- client/src/components/Icon/index.js
+++ client/src/components/Icon/index.js
@@ -2,12 +2,14 @@ import theme from './../../theme';
 
 // ICONS
 import Email from './icons/Email';
+import LeftRow from './icons/LeftRow';
 
 const Icon = (props) => {
   const { icon, color } = props;
 
   const IconMap = {
     email: Email,
+    leftRow: LeftRow,
   };
 
   const StyledIcon = IconMap[icon];
