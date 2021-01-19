99338848969c7fa21df6ba438489468f4b71d9f9
ramy <ramyshurafa@hotmail.com>
2021-01-19 17:05:15 +0200

    fix tests

--- server/database/dummy-data/users-completed-activities.js
+++ server/database/dummy-data/users-completed-activities.js
@@ -35,7 +35,7 @@ const createUserCompletedActivities = async (data) => {
   ];
 
   const JohnReeseCompletedMicrosoftWord = await createUserCompletedActivity(
-    completedActivities,
+    completedActivities[0],
   );
 
   return {
