b22d85c02f47e5b70b46d7186b2772c16080e75f
Fadi Omar <39485495+fadeomar@users.noreply.github.com>
2021-01-19 18:48:18 +0200

    40 create icon component (#85)
    * add icons to the app
    
    * add the new icons to the index
    
    * create stories for the icons
    
    * remove svg file

--- client/src/components/Icon/Icon.stories.js
+++ client/src/components/Icon/Icon.stories.js
@@ -20,3 +20,74 @@ Email.args = {
   width: 50,
   height: 50,
 };
+
+export const Planning = () => (
+  <Icon icon="planning" width={50} height={50} color="black" />
+);
+
+export const Office = () => (
+  <Icon icon="office" width={150} height={150} color="black" />
+);
+
+export const BrokenLamp = () => (
+  <Icon icon="brokenLamp" width={64} height={64} color="black" />
+);
+
+export const Camera = () => (
+  <Icon icon="camera" width={64} height={64} color="black" />
+);
+
+export const File = () => (
+  <Icon icon="file" width={64} height={64} color="black" />
+);
+
+export const TextFile = () => (
+  <Icon icon="textFile" width={64} height={64} color="black" />
+);
+
+export const Discussion = () => (
+  <Icon icon="discussion" width={64} height={64} color="black" />
+);
+
+export const ShareMedia = () => (
+  <Icon icon="shareMedia" width={64} height={64} color="black" />
+);
+
+export const Backup = () => (
+  <Icon icon="backup" width={64} height={64} color="black" />
+);
+
+export const Calendar = () => (
+  <Icon icon="calendar" width={64} height={64} color="black" />
+);
+
+export const Notebook = () => (
+  <Icon icon="notebook" width={64} height={64} color="black" />
+);
+
+export const Darts = () => (
+  <Icon icon="darts" width={172} height={150} color="black" />
+);
+
+export const Crown = () => (
+  <Icon icon="crown" width={172} height={150} color="black" />
+);
+
+export const Flash = () => (
+  <Icon
+    icon="flash"
+    width={120}
+    height={120}
+    color="liteTeal"
+    strokeColor="teal"
+  />
+);
+export const Cup = () => (
+  <Icon
+    icon="cup"
+    width={133}
+    height={131}
+    color="liteBlue"
+    strokeColor="navy"
+  />
+);
--- client/src/components/Icon/index.js
+++ client/src/components/Icon/index.js
@@ -3,6 +3,21 @@ import theme from './../../theme';
 // ICONS
 import Email from './icons/Email';
 import LeftRow from './icons/LeftRow';
+import Planning from './icons/Planning';
+import Office from './icons/Office';
+import BrokenLamp from './icons/BrokenLamp';
+import Camera from './icons/Camera';
+import File from './icons/File';
+import TextFile from './icons/TextFile';
+import Discussion from './icons/Discussion';
+import ShareMedia from './icons/ShareMedia';
+import Backup from './icons/Backup';
+import Calendar from './icons/Calendar';
+import Notebook from './icons/Notebook';
+import Darts from './icons/Darts';
+import Crown from './icons/Crown';
+import Flash from './icons/Flash';
+import Cup from './icons/Cup';
 
 const Icon = (props) => {
   const { icon, color } = props;
@@ -10,6 +25,21 @@ const Icon = (props) => {
   const IconMap = {
     email: Email,
     leftRow: LeftRow,
+    planning: Planning,
+    office: Office,
+    brokenLamp: BrokenLamp,
+    camera: Camera,
+    file: File,
+    textFile: TextFile,
+    discussion: Discussion,
+    shareMedia: ShareMedia,
+    backup: Backup,
+    calendar: Calendar,
+    notebook: Notebook,
+    darts: Darts,
+    crown: Crown,
+    flash: Flash,
+    cup: Cup,
   };
 
   const StyledIcon = IconMap[icon];
