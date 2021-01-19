4417d93de761b8a57e84376b1fd226294d549c97
ramy <ramyshurafa@hotmail.com>
2021-01-19 16:48:11 +0200

    create front end endpoints Relates #24

---
+++ client/src/api-calls/check-ins.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import handleError from './format-error';
+
+const CHECK_INS_BASE = '/skills';
+
+const createCheckIn = async ({ body, options }) => {
+  try {
+    const { data } = await axios.post(`${CHECK_INS_BASE}`, body);
+    return { data };
+  } catch (error) {
+    const err = handleError(error, options);
+    return { error: err };
+  }
+};
+
+export { createCheckIn };
