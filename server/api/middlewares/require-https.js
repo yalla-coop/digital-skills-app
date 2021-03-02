4db74ec39426e547c8be87379d6114a287a28bb8
Ahmed Shatat <a.shatat@hotmail.com>
2021-03-02 19:42:18 +0200

    fix require https to work on azure (#224)

--- server/api/middlewares/require-https.js
+++ server/api/middlewares/require-https.js
@@ -1,7 +1,7 @@
 function requireHTTPS(req, res, next) {
   // The 'x-forwarded-proto' check is for Heroku
   if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-    return res.redirect(`https://${req.get('host')}${req.url}`);
+    return res.redirect(`https://${req.get('host')}${process.env.PUBLIC_URL}${req.url}`);
   }
   next();
 }
